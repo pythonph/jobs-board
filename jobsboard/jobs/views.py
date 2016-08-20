7cc4a7252eeba591cd57d4ab1d8b5f20b6081ebb
D. Yu <dyu@fastmail.com>
2016-08-20 18:38:49 +0800

    jobs: add JobDetailView that maps to /jobs/<pk>/

--- jobsboard/jobs/views.py
+++ jobsboard/jobs/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Job
 
@@ -8,3 +9,9 @@ class JobListView(ListView):
     queryset = Job.objects.all()
     template_name = 'job_list.html'
     context_object_name = 'jobs'
+
+
+class JobDetailView(DetailView):
+    model = Job
+    context_object_name = 'job'
+    template_name = 'job_detail.html'
