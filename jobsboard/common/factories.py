1c790df5034f01413741b965b68ed7fd27e0d1ac
D. Yu <dyu@fastmail.com>
2016-08-20 19:12:02 +0800

    common: UserFactory

---
+++ jobsboard/common/factories.py
@@ -0,0 +1,10 @@
+import factory
+
+from django.contrib.auth.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker('email')
