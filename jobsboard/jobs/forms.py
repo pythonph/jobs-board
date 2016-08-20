5c1ac147331467b80e2f574e684c55c7a40a8023
Micaela Reyes <codemickeycode@gmail.com>
2016-08-20 23:09:28 +0800

    Update job post creation

--- jobsboard/jobs/forms.py
+++ jobsboard/jobs/forms.py
@@ -5,10 +5,6 @@ from .models import Job
 
 class JobForm(forms.ModelForm):
 
-    # class Meta:
-    #     model = Job
-    #     fields = ('title', 'creator',)
-
     class Meta:
         model = Job
-        exclude = ['created', 'updated', 'expiry']
+        exclude = ['creator', 'created', 'updated', 'expiry']
--- jobsboard/jobs/views.py
+++ jobsboard/jobs/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
 
 from .models import Job
 
@@ -24,3 +26,13 @@ class JobCreateView(CreateView):
     model = Job
     template_name = 'job_create.html'
     form_class = JobForm
+
+    def post(self, request, *args, **kwargs):
+        form = JobForm(request.POST)
+
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.creator = request.user
+            job.save()
+
+        return HttpResponse("Saved!")
