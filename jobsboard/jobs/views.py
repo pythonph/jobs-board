7448619aa5ac83ef645b40fd686274b03940508b
D. Yu <dyu@fastmail.com>
2016-08-20 17:46:08 +0800

    jobs: new view JobListView

--- jobsboard/jobs/views.py
+++ jobsboard/jobs/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Job
+
+
+class JobListView(ListView):
+    model = Job
+    queryset = Job.objects.all()
+    template_name = 'job_list.html'
+    context_object_name = 'jobs'
