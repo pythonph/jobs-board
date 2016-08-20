922d2bcce8695e51bdb186090a88d429cd5668a4
D. Yu <dyu@fastmail.com>
2016-08-20 17:46:56 +0800

    urls: include URLs from jobs app and namespace under /jobs/

--- jobsboard/urls.py
+++ jobsboard/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('jobsboard.web.urls')),
+    url(r'^jobs/', include('jobsboard.jobs.urls')),
 ]
