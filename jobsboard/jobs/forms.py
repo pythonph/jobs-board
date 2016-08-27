da9dd9e97a8c3e5568df1374dc6307de05bf5ccf
D. Yu <dyu@fastmail.com>
2016-08-27 16:10:16 +0800

    jobs: change expiry to expiry_date in JobForm.exclude

--- jobsboard/jobs/forms.py
+++ jobsboard/jobs/forms.py
@@ -7,4 +7,4 @@ class JobForm(forms.ModelForm):
 
     class Meta:
         model = Job
-        exclude = ['creator', 'created', 'updated', 'expiry']
+        exclude = ['creator', 'created', 'updated', 'expiry_date']
