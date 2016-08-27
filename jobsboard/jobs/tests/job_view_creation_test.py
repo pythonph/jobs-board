60835882536eb25f752ef3fea4c8230407a7f131
D. Yu <dyu@fastmail.com>
2016-08-27 16:24:14 +0800

    jobs: JobCreateViewTest.test_logged_in()

--- jobsboard/jobs/tests/job_view_creation_test.py
+++ jobsboard/jobs/tests/job_view_creation_test.py
@@ -1,7 +1,11 @@
 from django.test import Client, TestCase
 
+from jobsboard.common.factories import UserFactory
+
 
 class JobCreateViewTest(TestCase):
+
+    # TODO: test_create_logged_in()
 
     def test_logged_out(self):
         '''
@@ -12,3 +16,21 @@ class JobCreateViewTest(TestCase):
         client = Client()
         response = client.get('/jobs/new/', follow=True)
         self.assertRedirects(response, '/accounts/login/?next=/jobs/new/')
+
+    def test_logged_in(self):
+        '''
+        This checks if a logged out user visiting
+        the page for job creation is redirected to
+        the login page.
+        '''
+        user = UserFactory()
+        user.set_password('testpassword')
+        user.save()
+
+        client = Client()
+        login_success = client.login(username=user.username,
+                                     password='testpassword')
+        assert(login_success)
+
+        response = client.get('/jobs/new/', follow=True)
+        assert(response.status_code == 200)
