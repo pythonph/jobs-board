a324e280cfaf18bff6f55a5037b0ea535d803e45
D. Yu <dyu@fastmail.com>
2016-08-27 15:55:39 +0800

    jobs: require login to post jobs

--- jobsboard/jobs/views.py
+++ jobsboard/jobs/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
@@ -22,7 +23,7 @@ class JobDetailView(DetailView):
     template_name = 'job_detail.html'
 
 
-class JobCreateView(CreateView):
+class JobCreateView(LoginRequiredMixin, CreateView):
     model = Job
     template_name = 'job_create.html'
     form_class = JobForm
