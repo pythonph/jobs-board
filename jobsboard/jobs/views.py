d169de326e388171d4da16614335f75da381e610
D. Yu <dyu@fastmail.com>
2016-08-27 15:56:08 +0800

    jobs: remove unused imports in views.py

--- jobsboard/jobs/views.py
+++ jobsboard/jobs/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
-from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 from .models import Job
