d1a660be65afc12968422885d519ea413132b936
D. Yu <dyu@fastmail.com>
2016-08-27 14:29:33 +0800

    jobs: change default employment_type to full-time instead of part-time

--- jobsboard/jobs/tests/job_model_creation_test.py
+++ jobsboard/jobs/tests/job_model_creation_test.py
@@ -3,11 +3,12 @@ from django.test import TestCase
 from ..factories import JobFactory
 from ..models import Job
 
+
 class JobModelCreateTest(TestCase):
 
     def test_initial_state(self):
         job = JobFactory()
 
         assert(job.creator)
-        assert(job.is_featured == False)
-        assert(job.employment_type == Job.EMPLOYMENT_TYPE_PART_TIME)
+        assert(not job.is_featured)
+        assert(job.employment_type == Job.EMPLOYMENT_TYPE_FULL_TIME)
